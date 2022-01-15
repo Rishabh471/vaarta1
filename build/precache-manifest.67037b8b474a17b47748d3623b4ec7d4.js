cd6dd99dcf90a7ce59888d1a5abf65243300c3b8
Rishabh471 <64025471+Rishabh471@users.noreply.github.com>
2022-01-15 16:06:05 +0530

    Add files via upload

---
+++ build/precache-manifest.67037b8b474a17b47748d3623b4ec7d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad41d0c672a8a1b8b593aef112605078",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db019175fbfe3a7134a2",
+    "url": "/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "d35f0082d1e442d2f61a",
+    "url": "/static/js/2.c1aeafee.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/static/js/2.c1aeafee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db019175fbfe3a7134a2",
+    "url": "/static/js/main.ec6b5e79.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
